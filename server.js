8d3a4159fd1f53d0e9c4c4bf5fb1c9b2cc4d0141
wangrythmateus-bit <wangrythmateus@gmail.com>
2026-05-30 22:46:00 -0300

    Update express static middleware to use __dirname

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
     console.log('Uma tela foi conectada.');
